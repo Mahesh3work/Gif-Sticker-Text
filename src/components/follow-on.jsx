507335bec493cbcd2429ad7adf89d893ee0c438d
Mahesh3work <mahesh3work@gmail.com>
2025-01-01 23:13:00 +0530

    change link follow on

--- src/components/follow-on.jsx
+++ src/components/follow-on.jsx
@@ -1,4 +1,5 @@
 import {FaInstagram, FaXTwitter, FaYoutube} from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const FollowOn = () => {
   return (
@@ -7,15 +8,21 @@ const FollowOn = () => {
     >
       <span>Follow on:</span>
       <div className="flex gap-4 pt-3">
-        <a href="https://www.youtube.com/roadsidecoder">
+       <Link to="/">
+        <a href="#">
           <FaYoutube size={20} />
         </a>
-        <a href="https://www.instagram.com/roadsidecoder">
+      </Link>
+       <Link to="/">
+        <a href="#">
           <FaInstagram size={20} />
         </a>
-        <a href="https://www.twitter.com/piyush_eon">
+      </Link>
+       <Link to="/">
+        <a href="#">
           <FaXTwitter size={20} />
         </a>
+      </Link>
       </div>
     </div>
   );
