72b46dad33c2cfafed771d2c3a7e05fafb736f02
Mahesh3work <mahesh3work@gmail.com>
2025-01-01 18:46:03 +0530

    change 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
   
 import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AppLayout from './Layout/app-layout';
